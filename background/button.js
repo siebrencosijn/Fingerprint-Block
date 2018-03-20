1afffd846e1335fc3c7013cc564788857ebd8a44
nataliyaya <35452692+nataliyaya@users.noreply.github.com>
2018-03-20 13:27:30 +0100

    Delete button.js

--- background/button.js
+++
@@ -1,7 +0,0 @@
-function handleClick () {
-    browser.browserAction.setPopup(
-      //details https://developer.mozilla.org/en-US/Add-ons/WebExtensions/API/browserAction/setPopup
-    )  
-}
-
-browser.browserAction.onClicked.addListener(handleClick);
